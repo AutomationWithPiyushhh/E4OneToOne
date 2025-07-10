38b1423199eccdf542a0b802da7d64402dc120e2
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-07-10 11:49:17 +0530

    Relative Locator

---
+++ E4_Basic_Selenium/src/test/java/working_with_searchcontext/WorkingWithRL.java
@@ -0,0 +1,24 @@
+package working_with_searchcontext;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+public class WorkingWithRL {
+	public static void main(String[] args) {
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.facebook.com/r.php?entry_point=login");
+
+		driver.findElement(RelativeLocator
+				.with(By.cssSelector("input[aria-label='Surname']")) // desired element locator
+				.toRightOf(By.name("firstname")) // reference element locator
+				)
+				.sendKeys("Malhar");
+	}
+}

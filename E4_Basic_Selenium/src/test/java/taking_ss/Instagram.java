89713eba3c17e4039246295b6cecb9ed61d74398
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-07-25 17:58:37 +0530

    TKS of webelements

--- E4_Basic_Selenium/src/test/java/taking_ss/Instagram.java
+++ E4_Basic_Selenium/src/test/java/taking_ss/Instagram.java
@@ -8,23 +8,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
-public class Facebook {
+public class Instagram {
 	public static void main(String[] args) throws IOException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
-		driver.get("https://www.facebook.com/");
-
-//		Take the ss of a webpage
-		TakesScreenshot tks = (TakesScreenshot) driver;
-		File source1 = tks.getScreenshotAs(OutputType.FILE);	
-		File destination1 = new File("C:\\Users\\User\\git\\E4OneToOne\\E4_Basic_Selenium\\src\\test\\java\\taking_ss\\home1.png");
-	
-		FileHandler.copy(source1, destination1);
+				
+		driver.get("https://www.instagram.com/");
+		
+		WebElement login = driver.findElement(By.cssSelector("button[type='submit']"));
+		
+		File source = login.getScreenshotAs(OutputType.FILE);
+		File dest = new File("C:\\Users\\User\\git\\E4OneToOne\\E4_Basic_Selenium\\src\\test\\java\\taking_ss\\login.png");
+		
+		FileHandler.copy(source, dest);
 	}
-
 }
